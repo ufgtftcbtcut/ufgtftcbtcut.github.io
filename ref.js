c557bb3e66be5b01732ca5fa0222d63279b5b479
ufgtftcbtcut <103160162+ufgtftcbtcut@users.noreply.github.com>
2022-11-20 12:22:59 +0700

    Update ref.js

--- ref.js
+++ ref.js
@@ -2,7 +2,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 var data = datas[params.name]
-if(typeof params.name == "undefined") {
+if(params.name == null || window.location.search == "") {
   window.location.href = "https://ufgtftcbtcut.github.io/reference.md";
 }
 
