0ad5d75a95bc922f45bf020fd57d24639dceb714
ufgtftcbtcut <103160162+ufgtftcbtcut@users.noreply.github.com>
2022-11-20 11:56:37 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,22 @@
-
+data = {
+    "navbar":         ["Terminal","Output"],
+    "path":           "Main.Main.",
+    "name":           "Print",
+    "isFunction":     true,
+    "titleParams":    "String Text, Boolean InsertNewline = true",
+    "description":    "Prints text into the default terminal.",
+    "params":         [
+                        ["Text","What to print into the terminal","Required"],
+                        ["InsertNewline","Add a newline after the text?","true"]
+    ],
+    "returnValue":    "Nothing",
+    "example":        "print(\"Hello, world!\")",
+    "exampleComments":"print(\"Hello, world!\") # Prints Hello, world! into the terminal",
+    "exampleOutput":  "Hello, world!",
+    "notes":          "None",
+    "seeAlso":        [
+                        ["Input()","Take user input from the terminal"]
+    ],
+    "codedInC":       true,
+    "implementation": "Something"
+}
