bae69db886ebcd0adf8e1d94f7b596ecb8d807f6
ufgtftcbtcut <103160162+ufgtftcbtcut@users.noreply.github.com>
2022-12-04 10:18:03 +0700

    minor changes

--- refdata.js
+++ refdata.js
@@ -1,3 +1,9 @@
+// data converted from a private excel spreadsheet
+const none   = "None";
+const nthing = "Nothing";
+const tba    = "To be added";
+const change = "Might be changed";
+// variables for quick changing
 var datas = {
     "Print":{
         "navbar": ["Terminal","Output"],
@@ -10,15 +16,16 @@ var datas = {
             ["Text","What to print into the terminal","Required"],
             ["InsertNewline","Add a newline after the text?","true"]
         ],
-        "returnValue": "Nothing",
+        "returnValue": nthing,
         "example": "print(\"Hello, world!\")",
         "exampleComments": "print(\"Hello, world!\") # Prints Hello, world! into the terminal",
         "exampleOutput": "Hello, world!",
-        "notes": "None",
+        "notes": none,
         "seeAlso": [
-            ["Input()","Take user input from the terminal"]
+            ["Input()","Take user input from the terminal"],
+            ["Input.Masked()","Take password input from the terminal"]
         ],
         "codedInC": true,
-        "implementation": "Something"
+        "implementation": tba
     }
 }
